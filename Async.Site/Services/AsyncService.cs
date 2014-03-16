f48ba3a3afe326997445424aef25b2dfce0a4fe7
Josh Cadruvi <jcadruvi78@yahoo.com>
2014-03-16 09:27:40 -0700

    Adding the AsyncService.

---
+++ Async.Site/Services/AsyncService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Async.Services
+{
+    public class AsyncService
+    {
+        public void ThreadSleep ()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
